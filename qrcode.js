4c31fdbe23b0b831dce446e088e3f1c1c4cd920f
niButera <nikolasxp683gmail.com>
2024-03-18 02:47:59 -0300

    qrcode read only medicare

--- qrcode.js
+++ qrcode.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('qr-video');
 const canvas = document.getElementById('qr-canvas');
 const ctx = canvas.getContext('2d');
+let qrFound = false; // Variável para controlar se o QR foi encontrado
 
 const constraints = {
   video: {
@@ -17,7 +18,7 @@ navigator.mediaDevices.getUserMedia(constraints)
   });
 
 function tick() {
-  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+  if (!qrFound && video.readyState === video.HAVE_ENOUGH_DATA) {
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
@@ -27,11 +28,13 @@ function tick() {
     });
     if (code) {
       console.log('Código QR encontrado:', code.data);
+      qrFound = true; // Define para verdadeiro para evitar redirecionamento repetido
       // Redireciona para o link fornecido pelo código QR
       window.location.href = code.data;
     }
   }
   requestAnimationFrame(tick);
 }
+
 
 
