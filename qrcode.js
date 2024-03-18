73ab77ec4ba675e9a3788e79564731b65ed103c1
niButera <nikolasxp683gmail.com>
2024-03-18 02:26:15 -0300

    qrcode screen add, js add back, layout errors fixed

---
+++ qrcode.js
@@ -0,0 +1,36 @@
+const video = document.getElementById('qr-video');
+const canvas = document.getElementById('qr-canvas');
+const ctx = canvas.getContext('2d');
+
+const constraints = {
+  video: {
+    facingMode: { exact: "environment" }
+  }
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => {
+    video.srcObject = stream;
+    video.setAttribute('playsinline', true);
+    video.play();
+    requestAnimationFrame(tick);
+  });
+
+function tick() {
+  if (video.readyState === video.HAVE_ENOUGH_DATA) {
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const code = jsQR(imageData.data, imageData.width, imageData.height, {
+      inversionAttempts: 'dontInvert',
+    });
+    if (code) {
+      console.log('Código QR encontrado:', code.data);
+      
+      window.location.href = "https://nibutera.github.io/FrontMediCare/";
+    }
+  }
+  requestAnimationFrame(tick);
+}
+
